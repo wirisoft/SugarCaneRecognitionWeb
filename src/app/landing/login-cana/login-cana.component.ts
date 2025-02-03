1b2dfba867f5325f63f0b3484cc5557fcbcfa006
wirisoft <wirideveloper@gmail.com>
2025-02-02 19:53:17 -0600

    add login and register

--- src/app/landing/login-cana/login-cana.component.ts
+++ src/app/landing/login-cana/login-cana.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavBarCanaComponent } from '../components/nav-bar-cana/nav-bar-cana.component';
 import { FooterCanaComponent } from '../components/footer-cana/footer-cana.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-cana',
   standalone: true,
-  imports: [NavBarCanaComponent, FooterCanaComponent],
+  imports: [NavBarCanaComponent, FooterCanaComponent,
+    FormsModule,
+    CommonModule],
   templateUrl: './login-cana.component.html',
   styleUrl: './login-cana.component.css'
 })
 export class LoginCanaComponent {
+  loginData = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Login attempt with:', this.loginData);
+  }
 
 }
--- src/app/landing/register-cana/register-cana.component.ts
+++ src/app/landing/register-cana/register-cana.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 import { NavBarCanaComponent } from '../components/nav-bar-cana/nav-bar-cana.component';
 import { FooterCanaComponent } from '../components/footer-cana/footer-cana.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-cana',
   standalone: true,
-  imports: [NavBarCanaComponent, FooterCanaComponent, CommonModule],
+  imports: [NavBarCanaComponent, FooterCanaComponent, CommonModule,
+    FormsModule],
   templateUrl: './register-cana.component.html',
   styleUrl: './register-cana.component.css'
 })
 export class RegisterCanaComponent {
+  registerData = {
+    email: '',
+    password: '',
+    nombre: '',
+    apellidoPaterno: '',
+    apellidoMaterno: '',
+    telefono: ''
+  };
+
+  onSubmit() {
+    console.log('Register attempt with:', this.registerData);
+  }
   
 }
